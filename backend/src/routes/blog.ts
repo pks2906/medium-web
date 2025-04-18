48b20ea9f7e977238ea1b7e54ef7c5a88ee02dd1
pks2906 <pratik1971sinha@gmail.com>
2025-04-18 18:49:37 +0530

    added middleware

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,16 +1,36 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
+
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  const user = await verify(authHeader, c.env.JWT_SECRET);
+  if (user) {
+    //@ts-ignore
+    c.set("userId", user.id);
+    await next();
+  } else {
+    c.status(403);
+    return c.json({
+      message: "You are not logged in",
+    });
+  }
+});
 
 blogRouter.post("/", async (c) => {
   const body = await c.req.json();
+  const authorId = c.get("userId");
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
@@ -19,7 +39,7 @@ blogRouter.post("/", async (c) => {
     data: {
       title: body.title,
       content: body.content,
-      authorId: "1",
+      authorId: authorId,
     },
   });
 
@@ -79,6 +99,6 @@ blogRouter.get("/bulk", async (c) => {
   }).$extends(withAccelerate());
   const post = await prisma.post.findMany();
   return c.json({
-    post
-  })
+    post,
+  });
 });
