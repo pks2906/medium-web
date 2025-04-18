88df4a82033dbb69d15a7aa8d17fffaac1a685d1
pks2906 <pratik1971sinha@gmail.com>
2025-04-18 14:03:29 +0530

    added the signin endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,12 +25,30 @@ app.post("/api/v1/signup", async (c) => {
   const token = await sign({ id: user.id }, c.env.JWT_SECRET);
 
   return c.json({
-    jwt: token
+    jwt: token,
   });
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("Hello Hono!");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+  //@ts-ignore
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
 
 app.post("/api/v1/blog", (c) => {
