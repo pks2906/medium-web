026a9e27f583ef4dc0720e9087cf143614ea3410
pks2906 <pratik1971sinha@gmail.com>
2025-04-18 14:29:07 +0530

    added middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,31 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+
+  const header = c.req.header("authorization") || "";
+
+  // Bearer token
+  const token = header.split(" ")[1]
+
+  const response = await verify(token, c.env.JWT_SECRET)
+  if (response.id){
+    next()
+  } else{
+    c.status(403)
+    return c.json({error: "unauthorized"})
+  }
+  
+})
 
 app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
@@ -21,7 +39,7 @@ app.post("/api/v1/signup", async (c) => {
       password: body.password,
     },
   });
-  //@ts-ignore
+  
   const token = await sign({ id: user.id }, c.env.JWT_SECRET);
 
   return c.json({
@@ -46,7 +64,7 @@ app.post("/api/v1/signin", async (c) => {
     c.status(403);
     return c.json({ error: "user not found" });
   }
-  //@ts-ignore
+  
   const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
   return c.json({ jwt });
 });
